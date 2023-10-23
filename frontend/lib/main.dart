71620cec6b1e47141f38fd0d69ae9f1c0a2bc9d1
Victor Mateu <victormateulopez@gmail.com>
2023-10-23 18:24:08 +0200

    chore: firebase config

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:role_maister/config/app_router.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp()
   );
 }
