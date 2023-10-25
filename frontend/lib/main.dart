5cf4d29dcf41e68e60aec65d05a765c8c31038f4
Victor Mateu <victormateulopez@gmail.com>
2023-10-25 20:03:07 +0200

    fix: main

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:role_maister/config/app_router.dart';
-import 'package:role_maister/screens/game_screen.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -17,14 +16,9 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // title: 'Role Maister',
-      // theme: ThemeData(
-      //   primarySwatch: Colors.blue,
-      // ),
-      home: const GameScreen(),
-      // onGenerateRoute: AppRouter.onGenerateRoute,
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: ApplicationRouter().router
     );
   }
 }
-
