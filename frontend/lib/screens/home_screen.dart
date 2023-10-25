a5016535548b560bdf470aa23e5a96298f5a41d9
chaymaa dkouk <chaymaadkouk674@gmail.com>
2023-10-25 21:08:29 +0200

    done

--- frontend/lib/screens/home_screen.dart
+++ frontend/lib/screens/home_screen.dart
@@ -11,12 +11,12 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: const CustomAppBar(title: "Home"),
       drawer: customDrawer(context),
-      body: InitGame()//ListView(
-      //   children: const [
-      //     HomePage(),
-      //     WebFooter(),
-      //   ],
-      // )
+      body: ListView(
+        children: const [
+          HomePage(),
+          WebFooter(),
+        ],
+      )
     );
   }
 }
