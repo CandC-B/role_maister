f6479f62e9e51a758c96a28db393d813d5129808
MarioMontanuy <mariobnf58@gmail.com>
2023-11-08 18:34:17 +0100

    Fix: App bar titles

--- frontend/lib/screens/select_game_type_screen.dart
+++ frontend/lib/screens/select_game_type_screen.dart
@@ -12,14 +12,14 @@ class SelectGameTypeScreen extends StatelessWidget {
     bool mobile = MediaQuery.of(context).size.width > 700 ? false : true;
     if (mobile) {
       return Scaffold(
-          appBar: const CustomAppBar(title: "none"),
+          appBar: const CustomAppBar(title: "Game Select"),
           drawer: customDrawer(context),
           body: ListView(
             children: const [SelectGameType()],
           ));
     } else {
       return Scaffold(
-          appBar: const CustomAppBar(title: "none"),
+          appBar: const CustomAppBar(title: "Game Select"),
           drawer: customDrawer(context),
           body: ListView(
             children: const [
