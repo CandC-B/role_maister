a5ba1a5d7baee58dbcdd2a07ecf1d4d15ac718d3
SaraSerrano7 <sasecu@gmail.com>
2024-01-05 01:47:36 +0100

    WIP: cthulhu random character sheet

--- frontend/lib/models/cthulhu_character.dart
+++ frontend/lib/models/cthulhu_character.dart
@@ -8,76 +8,278 @@ class CthulhuCharacter extends Character {
   // TODO: de momento solo tenemos character level 1
   
   final int characterLevel;
-  final String career;
-  final Map<String, int> attributes;
-  final Map<String, int> skills;
-  final List<String> talents;
-  final String appearance;
-  final String personalAgenda;
-  final String friend;
-  final String rival;
-  final List<String> gear;
-  final String signatureItem;
   final String? photoUrl;
-  final int cash;
-  final int hp;
   final String mode = "cthulhu";
+
+  final String occupation;
+  final String gender;
+  final int age;
+  final Map<String, int> characteristics;
+  final int bonusDamage;
+  final int hp;
+  final int sanity;
+  final int mp;
+  final int luck;
+  final Map<String, int> skills;
+  final Map<String, int> weapons;
+  final List<String> personalDescription;
+  final String ideology;
+  final String relatives;
+  final String significantPlaces;
+  final String prizedPossessions;
+  final String traits;
+  final String phobias;
+  final String manias;
+  final List<String> equipment;
 
   CthulhuCharacter({
     String? id,
     required this.characterLevel,
-    required this.career,
-    required this.attributes,
-    required this.skills,
-    required this.talents,
     required String name,
     required String userId,
-    required this.appearance,
-    required this.personalAgenda,
-    required this.friend,
-    required this.rival,
-    required this.gear,
-    required this.signatureItem,
-    required this.cash,
     required this.photoUrl,
+    
+    required this.occupation,
+    required this.gender,
+    required this.age,
+    required this.characteristics,
+    required this.bonusDamage,
     required this.hp,
+    required this.sanity,
+    required this.mp,
+    required this.luck,
+    required this.skills,
+    required this.weapons,
+    required this.personalDescription,
+    required this.ideology,
+    required this.relatives,
+    required this.significantPlaces,
+    required this.prizedPossessions,
+    required this.traits,
+    required this.phobias,
+    required this.manias,
+    required this.equipment,
   }) : super(name, userId , id: id);
 
   // Factory constructor to generate random AliensCharacter
   factory CthulhuCharacter.random() {
-    final career = _generateCareer();
-    final attributes = _generateRandomAttributes(career);
-    final skills = _generateRandomSkills(career);
-    final talents = _generateRandomTalents(career);
-    String name = _getRandomName(career);
+    final occupation = _generateRandomOccupation();
+    final gender = _generateRandomGender();
+    final age = _generateRandomAge();
+    final characteristics = _generateRandomCharacteristics();
+    final bonusDamage = _getRandomBonusDamage(characteristics);
+    final hp = _getRandomHp(characteristics);
+    final sanity = _getRandomSanity(characteristics);
+    final mp = _getRandomMp(characteristics);
+    final luck = _getRandomLuck();
+    final skills = _getRandomSkills(occupation);
+    final weapons = _getRandomWeapons();
+    final personalDescription = _getRandomPersonalDescription(gender);
+    final ideology = _getRandomIdeology();
+    final relatives = _getRandomRelatives();
+    final significantPlaces = _getRandomSignificantPlaces();
+    final prizedPossessions = _getRandomPrizedPossessions();
+    final traits = _getRandomTraits();
+    final phobias = _getRandomPhobias();
+    final manias = _getRandomManias();
+    final equipment = _getRandomEquipment();
+    
+    String name = _getRandomName(gender);
     String userId = "test";
-    final appearance = _getRandomAppearance(career);
-    final personalAgenda = _getRandomPersonalAgenda(career);
-    final friend = _getRandomName(career);
-    final enemy = _getRandomName(career);
-    final gear = _getRandomGear(career);
-    final signatureItem = _getRandomSignatureItem(career);
-    final cash = _getRandomCash(career);
-    final photoUrl = "small_logo.png";
+    const photoUrl = "small_logo.png";
     return CthulhuCharacter(
       characterLevel: 1,
-      career: career,
-      attributes: attributes,
-      skills: skills,
-      talents: talents,
       name: name,
-      appearance: appearance,
-      personalAgenda: personalAgenda,
-      friend: friend,
-      rival: enemy,
-      gear: gear,
-      signatureItem: signatureItem,
-      cash: cash,
-      photoUrl: photoUrl,
-      hp: 2 * attributes["Strength"]!,
       userId: userId,
+      photoUrl: photoUrl,
+      occupation: occupation,
+      gender: gender,
+      age: age,
+      characteristics: characteristics,
+      bonusDamage: bonusDamage,
+      hp: hp,
+      sanity: sanity,
+      mp: mp,
+      luck: luck,
+      skills: skills,
+      weapons: weapons,
+      personalDescription: personalDescription,
+      ideology: ideology,
+      relatives: relatives,
+      significantPlaces: significantPlaces,
+      prizedPossessions: prizedPossessions,
+      traits: traits,
+      phobias: phobias,
+      manias: manias,
+      equipment: equipment,
     );
   }
+
+
+  static String _generateRandomOccupation() {
+    List<String> occupations = [
+      "Antiquarian",
+      "Dilettante",
+      "Writer",
+      "Police inspector",
+      "Private investigator",
+      "Medic",
+      "Journalist",
+      "University professor",
+    ];
+    return randomChoice(occupations);
+  }
+
+  static String _generateRandomGender() {
+    return randomChoice(["Male", "Female"]);
+  }
+
+  static String _getRandomName(String gender) {
+    if(gender == "Male") {
+      return randomChoice(["Nevada Jones", "Wentworth Avebury", "Mike Hawk"]);
+    } else {
+      return randomChoice(["Jessie Williams", "Keiko Cain", "Lois Russo"]);
+    }
+  }
+
+  static int _generateRandomAge() {
+    return Random().nextInt(63) + 18;
+  }
+
+  static Map<String, int> _generateRandomCharacteristics() {
+    Map<String, int> characteristics = {
+      "STR": 0,
+      "CON": 0,
+      "POW": 0,
+      "DEX": 0,
+      "APP": 0,
+      "SIZ": 0,
+      "INT": 0,
+      "EDU": 0,
+    };
+
+    List<int> probs = [40, 50, 50, 50, 60, 60, 70, 80];
+    characteristics.forEach((_, percent) {
+      int prob = randomChoice(probs);
+      percent = prob;
+      probs.remove(prob);
+    });
+
+    return characteristics;
+  }
+
+  static int _getRandomBonusDamage(Map<String, int> characteristics) {
+    int sum = characteristics['STR']! + characteristics['SIZ']!;
+    if(sum >= 2 && sum <= 64) {
+      return -2;
+    } else if(sum >= 65 && sum <= 84) {
+      return -1;
+    } else if(sum >= 85 && sum <= 124) {
+      return 0;
+    } else if(sum >= 125 && sum <= 164) {
+      return makeRoll(1, 4);
+    } else {
+      return makeRoll(1, 6);
+    }
+  }
+
+  static int makeRoll(int times, int dice) {
+    int result = 1;
+    for (int i = 0; i < times; i++) {
+      result += Random().nextInt(dice + 1);
+    }
+    return result;
+  }
+
+  static int _getRandomHp(Map<String, int> characteristics) {
+    int sum = characteristics['STR']! + characteristics['CON']!;
+    return (sum/10).floor();
+  }
+
+  static int _getRandomSanity(Map<String, int> characteristics) {
+    return characteristics['POW']!;
+  }
+
+  static int _getRandomMp(Map<String, int> characteristics) {
+    return characteristics['POW']!;
+  }
+
+  static int _getRandomLuck() {
+    return makeRoll(3, 6) * 5;
+  }
+
+  static Map<String, int> _getRandomSkills(String occupation) {
+    Map<String, Map<String, int>> skills = {
+      "Antiquarian": {
+        "Appraise": 0,
+        "Art/Craft": 0,
+        "History": 0,
+        "Language (other)": 0,
+        "Library Use": 0,
+        
+      },
+    };
+    if(occupation == 'Antiquarian') {
+
+    }
+    
+    return Map<String, int>();
+  }
+
+  static final Map<String, int> _randomSkills = {
+    "Accounting": 05,
+    "Anthropology": 01,
+    "Appraise": 05,
+    "Archeology": 01,
+    "Art/Craft": 05,
+    "Charm": 15,
+    "Climb": 20,
+    "Credit Rating": 00,
+    "Disguise": 05,
+    "Dodge": characteristics['DEX']!/2.floor(),
+    
+  };
+
+  static Map<String, int> _getRandomWeapons() {
+    return Map<String, int>();
+  }
+
+  static List<String> _getRandomPersonalDescription(String gender) {
+    return [""].toList();
+  }
+
+  static String _getRandomIdeology() {
+    return "";
+  }
+
+  static String _getRandomRelatives() {
+    return "";
+  }
+
+  static String _getRandomSignificantPlaces() {
+    return "";
+  }
+
+  static String _getRandomPrizedPossessions() {
+    return "";
+  }
+
+  static String _getRandomTraits() {
+    return "";
+  }
+
+  static String _getRandomPhobias() {
+    return "";
+  }
+
+  static String _getRandomManias() {
+    return "";
+  }
+
+  static List<String> _getRandomEquipment() {
+    return [""].toList();
+  }
+
 
   static String _generateCareer() {
     List<String> careers = [
@@ -173,7 +375,7 @@ class CthulhuCharacter extends Character {
     throw Exception("Unsupported career selected");
   }
 
-  static String _getRandomName(String selectedCareer) {
+  static String _getRandomName2(String selectedCareer) {
     Map<String, List<String>> names = {
       "Colonial marine": [
         "Marcus Mullaney",
@@ -566,20 +768,29 @@ class CthulhuCharacter extends Character {
         ' id: $id,'
         ' mode: $mode,'
         ' characterLevel: $characterLevel,'
-        ' career: $career,'
-        ' attributes: $attributes,'
-        ' skills: $skills,'
-        ' talents: $talents,'
         ' name: $name,'
-        ' appearance: $appearance,'
-        ' personalAgenda: $personalAgenda,'
-        ' friend: $friend,'
-        ' rival: $rival,'
-        ' gear: $gear,'
         'photoUrl: $photoUrl,'
-        ' signatureItem: $signatureItem,'
-        ' cash: $cash,'
-        ' hp: $hp'
+
+        ' occupation: $occupation,'
+        ' gender: $gender,'
+        ' age: $age,'
+        ' characteristics: $characteristics,'
+        ' bonusDamage: $bonusDamage,'
+        ' hp: $hp,'
+        ' sanity: $sanity,'
+        ' mp: $mp,'
+        ' luck: $luck,'
+        ' skills: $skills,'
+        ' weapons: $weapons,'
+        ' personalDescription: $personalDescription,'
+        ' ideology: $ideology,'
+        ' relatives: $relatives,'
+        ' significantPlaces: $significantPlaces,'
+        ' prizedPossessions: $prizedPossessions,'
+        ' traits: $traits,'
+        ' phobias: $phobias,'
+        ' manias: $manias,'
+        ' equipment: $equipment'
         '}';
   }
 
@@ -589,48 +800,67 @@ class CthulhuCharacter extends Character {
       'id': id,
       'mode': mode,
       'character_level': characterLevel,
-      'career': career,
-      'attributes': attributes,
-      'skills': skills,
-      'talents': talents,
       'name': name,
-      'appearance': appearance,
-      'personal_agenda': personalAgenda,
-      'friend': friend,
-      'rival': rival,
-      'gear': gear,
       'photoUrl': photoUrl,
-      'signature_item': signatureItem,
-      'cash': cash,
+      
+      'occupation': occupation,
+      'gender': gender,
+      'age': age,
+      'characteristics': characteristics,
+      'bonusDamage': bonusDamage,
       'hp': hp,
+      'sanity': sanity,
+      'mp': mp,
+      'luck': luck,
+      'skills': skills,
+      'weapons': weapons,
+      'personalDescription': personalDescription,
+      'ideology': ideology,
+      'relatives': relatives,
+      'significantPlaces': significantPlaces,
+      'prizedPossessions': prizedPossessions,
+      'traits': traits,
+      'phobias': phobias,
+      'manias': manias,
+      'equipment': equipment,
     };
   }
 
   static fromMap(Map<String, dynamic> statsData) {
     return CthulhuCharacter(
+      characterLevel: statsData['character_level'] as int,
       userId: statsData['userId'] as String,
       id: statsData['id'] as String,
       name: statsData['name'] as String,
-      hp: statsData['hp'] as int,
-      characterLevel: statsData['character_level'] as int,
-      career: statsData['career'] as String,
-      attributes: (statsData['attributes'] as Map<String, dynamic>)
+      photoUrl: statsData['photoUrl'] as String,
+
+      occupation: statsData['occupation'] as String,
+      gender: statsData['gender'] as String,
+      age: statsData['age'] as int,
+      characteristics: (statsData['characteristics'] as Map<String, dynamic>)
           .map((key, value) => MapEntry(key, value as int)),
+      bonusDamage: statsData['bonusDamage'] as int,
+      hp: statsData['hp'] as int,
+      sanity: statsData['sanity'] as int,
+      mp: statsData['mp'] as int,
+      luck: statsData['luck'] as int,
       skills: (statsData['skills'] as Map<String, dynamic>)
           .map((key, value) => MapEntry(key, value as int)),
-      talents: (statsData['talents'] as List<dynamic>)
+      weapons: (statsData['weapons'] as Map<String, dynamic>)
+          .map((key, value) => MapEntry(key, value as int)),
+      personalDescription: (statsData['personalDescription'] as List<dynamic>)
           .map((value) => value as String)
           .toList(),
-      appearance: statsData['appearance'] as String,
-      personalAgenda: statsData['signature_item'] as String,
-      friend: statsData['friend'] as String,
-      rival: statsData['rival'] as String,
-      gear: (statsData['gear'] as List<dynamic>)
+      ideology: statsData['ideology'] as String,
+      relatives: statsData['relatives'] as String,
+      significantPlaces: statsData['significantPlaces'] as String,
+      prizedPossessions: statsData['prizedPossessions'] as String,
+      traits: statsData['traits'] as String,
+      phobias: statsData['phobias'] as String,
+      manias: statsData['manias'] as String,
+      equipment: (statsData['equipment'] as List<dynamic>)
           .map((value) => value as String)
           .toList(),
-      photoUrl: statsData['photoUrl'] as String,
-      signatureItem: statsData['signature_item'] as String,
-      cash: statsData['cash'] as int,
     );
   }
 }
