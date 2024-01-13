dc4124de5c051967d477a93d8ff97ea920d0c601
Victor Mateu <victormateulopez@gmail.com>
2024-01-13 18:58:49 +0100

    fix: no leading in players widget when device is web

--- frontend/lib/widgets/game_players.dart
+++ frontend/lib/widgets/game_players.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:role_maister/models/game.dart';
@@ -57,6 +58,7 @@ class _GamePlayersState extends State<GamePlayers> {
             length: 2,
             child: Scaffold(
               appBar: AppBar(
+                automaticallyImplyLeading: kIsWeb ? false : true,
                 actions: [
                   IconButton(
                     icon: const Icon(Icons.exit_to_app),
